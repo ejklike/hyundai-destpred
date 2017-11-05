86416de9d1355836cecfd6fd4a8f4ea664303309
juung <mjihyung@gmail.com>
2017-11-05 23:43:21 +0900

    add dest_cluster option

--- estimator.py
+++ estimator.py
@@ -5,14 +5,15 @@ import os
 import pickle
 
 import numpy as np
+from sklearn.cluster import MeanShift
 import tensorflow as tf
 
 from data_preprocessor import DataPreprocessor
 from log import log
 from models import model_fn
 from utils import (maybe_exist,
-                   get_pkl_file_name, 
-                   load_data, 
+                   get_pkl_file_name,
+                   load_data,
                    record_results,
                    visualize_predicted_destination)
 
@@ -30,246 +31,256 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 
 def train_and_eval(car_id, proportion, dest_term, model_id, params, viz=False):
-  # Load datasets
-  fname_trn = os.path.join(
-      DATA_DIR,
-      get_pkl_file_name(car_id, proportion, dest_term, train=True))
-  fname_tst = os.path.join(
-      DATA_DIR,
-      get_pkl_file_name(car_id, proportion, dest_term, train=False))
+    # Load datasets
+    fname_trn = os.path.join(
+        DATA_DIR,
+        get_pkl_file_name(car_id, proportion, dest_term, train=True))
+    fname_tst = os.path.join(
+        DATA_DIR,
+        get_pkl_file_name(car_id, proportion, dest_term, train=False))
 
-  path_trn, meta_trn, dest_trn = load_data(fname_trn, k=params['k'])
-  path_tst, meta_tst, dest_tst = load_data(fname_tst, k=params['k'])
-  log.info('trn_data_size, tst_data_size: ' + str(path_trn.shape) + ', ' + str(path_tst.shape))
+    path_trn, meta_trn, dest_trn = load_data(fname_trn, k=params['k'])
+    path_tst, meta_tst, dest_tst = load_data(fname_tst, k=params['k'])
+    log.info('trn_data_size, tst_data_size: ' + str(path_trn.shape) + ', ' + str(path_tst.shape))
 
-  print(path_trn[0], path_tst[0])
+    print(path_trn[0], path_tst[0])
 
-  # data for feeding to the graph
-  input_dict_trn, input_dict_tst = {}, {}
-  if params['use_meta']:
-    input_dict_trn['meta'] = meta_trn
-    input_dict_tst['meta'] = meta_tst
-  if params['use_path']:
-    input_dict_trn['path'] = path_trn
-    input_dict_tst['path'] = path_tst
+    # data for feeding to the graph
+    input_dict_trn, input_dict_tst = {}, {}
+    if params['use_meta']:
+        input_dict_trn['meta'] = meta_trn
+        input_dict_tst['meta'] = meta_tst
+    if params['use_path']:
+        input_dict_trn['path'] = path_trn
+        input_dict_tst['path'] = path_tst
+    if params['use_cluster']:
+        ms = MeanShift()
+        ms.fit(dest_trn)
+        input_dict_trn['dest_centroid'] = np.repeat(np.expand_dims(ms.cluster_centers_, axis=0), path_trn.shape[0], axis=0).astype(np.float32)
+        input_dict_tst['dest_centroid'] = np.repeat(np.expand_dims(ms.cluster_centers_, axis=0), path_tst.shape[0], axis=0).astype(np.float32)
 
-  # Instantiate Estimator
-  model_dir = os.path.join(MODEL_DIR, model_id)
-  config = tf.estimator.RunConfig().replace(
-      # log_step_count_steps=10,
-      save_summary_steps=10,)
-  nn = tf.estimator.Estimator(
-      model_fn=model_fn,
-      params=params,
-      config=config,
-      model_dir=model_dir)
 
-  # Train
-  if FLAGS.train:
-    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    # Instantiate Estimator
+    model_dir = os.path.join(MODEL_DIR, model_id)
+    config = tf.estimator.RunConfig().replace(
+        # log_step_count_steps=10,
+        save_summary_steps=10, )
+    nn = tf.estimator.Estimator(
+        model_fn=model_fn,
+        params=params,
+        config=config,
+        model_dir=model_dir)
+
+    # Train
+    if FLAGS.train:
+        train_input_fn = tf.estimator.inputs.numpy_input_fn(
+            x=input_dict_trn,
+            y=dest_trn,
+            batch_size=path_trn.shape[0],
+            num_epochs=None,
+            shuffle=True)
+        nn.train(input_fn=train_input_fn, steps=FLAGS.steps)
+
+    # Eval
+    eval_input_fn_trn = tf.estimator.inputs.numpy_input_fn(
         x=input_dict_trn,
         y=dest_trn,
-        batch_size=path_trn.shape[0],
-        num_epochs=None,
-        shuffle=True)
-    nn.train(input_fn=train_input_fn, steps=FLAGS.steps)
+        num_epochs=1,
+        shuffle=False)
+    eval_input_fn_tst = tf.estimator.inputs.numpy_input_fn(
+        x=input_dict_tst,
+        y=dest_tst,
+        num_epochs=1,
+        shuffle=False)
 
-  # Eval
-  eval_input_fn_trn = tf.estimator.inputs.numpy_input_fn(
-      x=input_dict_trn,
-      y=dest_trn,
-      num_epochs=1,
-      shuffle=False)
-  eval_input_fn_tst = tf.estimator.inputs.numpy_input_fn(
-      x=input_dict_tst,
-      y=dest_tst,
-      num_epochs=1,
-      shuffle=False)
+    # Scores
+    ev_trn = nn.evaluate(input_fn=eval_input_fn_trn)
+    ev_tst = nn.evaluate(input_fn=eval_input_fn_tst)
+    log.info("Loss (trn, tst): {:.3f}, {:.3f}".format(ev_trn["loss"], ev_tst['loss']))
 
-  # Scores
-  ev_trn = nn.evaluate(input_fn=eval_input_fn_trn)
-  ev_tst = nn.evaluate(input_fn=eval_input_fn_tst)
-  log.info("Loss (trn, tst): {:.3f}, {:.3f}".format(ev_trn["loss"], ev_tst['loss']))
+    # Viz
+    if viz:
+        maybe_exist(VIZ_DIR)
+        pred_tst = nn.predict(input_fn=eval_input_fn_tst)
+        pred_tst = islice(pred_tst, 10)
+        for i, pred in enumerate(pred_tst):
+            fname = '{}/{}__tst_{}.png'.format(VIZ_DIR, model_id, i)
+            visualize_predicted_destination(
+                path_tst[i], dest_tst[i], pred, fname=fname)
 
-  # Viz
-  if viz:
-    maybe_exist(VIZ_DIR)
-    pred_tst = nn.predict(input_fn=eval_input_fn_tst)
-    pred_tst = islice(pred_tst, 10)
-    for i, pred in enumerate(pred_tst):
-      fname = '{}/{}__tst_{}.png'.format(VIZ_DIR, model_id, i)
-      visualize_predicted_destination(
-          path_tst[i], dest_tst[i], pred, fname=fname)
-
-  return ev_trn['loss'], ev_tst['loss']
+    return ev_trn['loss'], ev_tst['loss']
 
 
 def main(_):
-  # Preprocess data: convert to pkl data
-  if FLAGS.preprocess:
-    maybe_exist(DATA_DIR)
-    data_preprocessor = DataPreprocessor(RAW_DATA_FNAME)
-    data_preprocessor.process_and_save(save_dir=DATA_DIR)
+    # Preprocess data: convert to pkl data
+    if FLAGS.preprocess:
+        maybe_exist(DATA_DIR)
+        data_preprocessor = DataPreprocessor(RAW_DATA_FNAME)
+        data_preprocessor.process_and_save(save_dir=DATA_DIR)
 
-    # (1) 주행경로 길이의 평균
-    # (2) 주행 횟수
-    #
-    #       (1)    (2)
-    #   5: 상위권 하위권
-    # 100: 상위권 하위권
-    #  29: 상위권 하위권
-    #  72: 하위권 상위권
-    #  50: 하위권 상위권
-    #  14:  평균  상위권
-    #   9:  평균   평균
-    #  74:  평균   평균
+        # (1) 주행경로 길이의 평균
+        # (2) 주행 횟수
+        #
+        #       (1)    (2)
+        #   5: 상위권 하위권
+        # 100: 상위권 하위권
+        #  29: 상위권 하위권
+        #  72: 하위권 상위권
+        #  50: 하위권 상위권
+        #  14:  평균  상위권
+        #   9:  평균   평균
+        #  74:  평균   평균
 
-  # input specification
-  car_id_list = [100]#[5, 9, 14, 29, 50, 72, 74, 100]
-  proportion_list = [0.4]#[0.2, 0.4, 0.6, 0.8]
-  short_term_dest_list = [5]#[-1, 5]
-  
-  # Used for loading data and building graph
-  use_meta_path = [(False, True)]#, (True, False), (False, True)]
-  if FLAGS.model_type == 'dnn':
-    k_list = [5]#, 10, 15, 20]
-    bi_direction_list = [False]
-  else:
-    k_list = [0]
-    bi_direction_list = [True, False]
-  
-  path_embedding_dim_list = [16]
-  n_hidden_layer_list = [1]
+    # input specification
+    car_id_list = [100]  # [5, 9, 14, 29, 50, 72, 74, 100]
+    proportion_list = [0.4]  # [0.2, 0.4, 0.6, 0.8]
+    short_term_dest_list = [5]  # [-1, 5]
+    use_cluster_dest = [False, True]
 
-  # PARAM GRIDS
-  param_product = product(car_id_list, 
-                          proportion_list, 
-                          short_term_dest_list, 
-                          use_meta_path,
-                          k_list,
-                          bi_direction_list,
-                          path_embedding_dim_list,
-                          n_hidden_layer_list)
-  param_product_size = np.prod([len(car_id_list),
-                                len(proportion_list),
-                                len(short_term_dest_list),
-                                len(use_meta_path),
-                                len(k_list),
-                                len(bi_direction_list), 
-                                len(path_embedding_dim_list), 
-                                len(n_hidden_layer_list)])
+    # Used for loading data and building graph
+    use_meta_path = [(False, True)]  # , (True, False), (False, True)]
+    if FLAGS.model_type == 'dnn':
+        k_list = [5]  # , 10, 15, 20]
+        bi_direction_list = [False]
+    else:
+        k_list = [0]
+        bi_direction_list = [True, False]
 
-  for i, params in enumerate(param_product):
-    car_id, proportion, dest_term = params[:3]
-    use_meta, use_path = params[3]
-    k, bi_direction, path_embedding_dim, n_hidden_layer = params[4:]
+    path_embedding_dim_list = [16]
+    n_hidden_layer_list = [1]
 
-    # If we do not use path input,
-    # some param grids are not needed.
-    if use_path is False:
-      if proportion > proportion_list[0]:
-        continue
-      if (FLAGS.model_type == 'dnn') and (k > k_list[0]):
-        continue
-      # this param is useless
-      path_embedding_dim = 0
+    # PARAM GRIDS
+    param_product = product(car_id_list,
+                            proportion_list,
+                            short_term_dest_list,
+                            use_cluster_dest,
+                            use_meta_path,
+                            k_list,
+                            bi_direction_list,
+                            path_embedding_dim_list,
+                            n_hidden_layer_list)
+    param_product_size = np.prod([len(car_id_list),
+                                  len(proportion_list),
+                                  len(short_term_dest_list),
+                                  len(use_cluster_dest),
+                                  len(use_meta_path),
+                                  len(k_list),
+                                  len(bi_direction_list),
+                                  len(path_embedding_dim_list),
+                                  len(n_hidden_layer_list)])
 
-    model_params = dict(
-        learning_rate=FLAGS.learning_rate,
-        # feature_set
-        use_meta=use_meta,
-        use_path=use_path,
-        # model type
-        model_type=FLAGS.model_type,
-        # rnn params
-        bi_direction=bi_direction,
-        # dnn params
-        k=k,
-        # path embedding dim (rnn: n_unit / dnn: out_dim)
-        path_embedding_dim=path_embedding_dim,
-        # the num of final dense layers
-        n_hidden_layer=n_hidden_layer,
-    )
+    for i, params in enumerate(param_product):
+        car_id, proportion, dest_term, use_cluster = params[:4]
+        use_meta, use_path = params[4]
+        k, bi_direction, path_embedding_dim, n_hidden_layer = params[5:]
 
-    # Model id
-    model_id = 'car_{:03}'.format(car_id)
-    model_id += '__prop_{}'.format(proportion)
-    model_id += '__dest_{}'.format(dest_term)
-    model_id += '__' + ''.join(['meta' if use_meta else '____', 
-                                'path' if use_path else '____'])
-    model_id += '__' + ''.join(['b' if bi_direction else '', 
-                                FLAGS.model_type])
-    model_id += '__k_{}_pdim_{}_dense_{}'.format(k, 
-                                                 path_embedding_dim, 
-                                                 n_hidden_layer)
+        # If we do not use path input,
+        # some param grids are not needed.
+        if use_path is False:
+            if proportion > proportion_list[0]:
+                continue
+            if (FLAGS.model_type == 'dnn') and (k > k_list[0]):
+                continue
+            # this param is useless
+            path_embedding_dim = 0
 
-    log.warning('=' * 30 + '{} / {} ({:.1f}%)'.format(
-        i + 1, param_product_size, (i + 1) / param_product_size * 100) + '=' * 30)
-    log.warning('model_id: ' + model_id)
-    log.warning('Using params: ' + str(model_params))
+        model_params = dict(
+            learning_rate=FLAGS.learning_rate,
+            # feature_set
+            use_meta=use_meta,
+            use_path=use_path,
+            use_cluster=use_cluster,
+            # model type
+            model_type=FLAGS.model_type,
+            # rnn params
+            bi_direction=bi_direction,
+            # dnn params
+            k=k,
+            # path embedding dim (rnn: n_unit / dnn: out_dim)
+            path_embedding_dim=path_embedding_dim,
+            # the num of final dense layers
+            n_hidden_layer=n_hidden_layer,
+        )
 
-    trn_rmse, tst_rmse = train_and_eval(car_id, proportion, dest_term, 
-                                        model_id, model_params)
-    if FLAGS.train:
-      record_results(RECORD_FNAME, model_id, trn_rmse, tst_rmse)
-    
+        # Model id
+        model_id = 'car_{:03}'.format(car_id)
+        model_id += '__prop_{}'.format(proportion)
+        model_id += '__dest_{}'.format(dest_term)
+        model_id += '__cluster_{}'.format('T' if use_cluster else 'F')
+        model_id += '__' + ''.join(['meta' if use_meta else '____',
+                                    'path' if use_path else '____'])
+        model_id += '__' + ''.join(['b' if bi_direction else '',
+                                    FLAGS.model_type])
+        model_id += '__k_{}_pdim_{}_dense_{}'.format(k,
+                                                     path_embedding_dim,
+                                                     n_hidden_layer)
+
+        log.warning('=' * 30 + '{} / {} ({:.1f}%)'.format(
+            i + 1, param_product_size, (i + 1) / param_product_size * 100) + '=' * 30)
+        log.warning('model_id: ' + model_id)
+        log.warning('Using params: ' + str(model_params))
+
+        trn_rmse, tst_rmse = train_and_eval(car_id, proportion, dest_term,
+                                            model_id, model_params)
+        if FLAGS.train:
+            record_results(RECORD_FNAME, model_id, trn_rmse, tst_rmse)
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-  # Model setting
-  parser.add_argument(
-      'model_type', 
-      type=str, 
-      default='dnn',
-      help='dnn/rnn')
+    # Model setting
+    parser.add_argument(
+        'model_type',
+        type=str,
+        default='dnn',
+        help='dnn/rnn')
 
-  # Data preprocess
-  parser.add_argument(
-      '--preprocess', 
-      type=bool, 
-      nargs='?',
-      default=False, #default
-      const=True, #if the arg is given
-      help='Preprocess data or not')
+    # Data preprocess
+    parser.add_argument(
+        '--preprocess',
+        type=bool,
+        nargs='?',
+        default=False,  # default
+        const=True,  # if the arg is given
+        help='Preprocess data or not')
 
-  # gpu allocation
-  parser.add_argument(
-      '--gpu_no', 
-      type=str, 
-      default=None,
-      help='gpu device number')
+    # gpu allocation
+    parser.add_argument(
+        '--gpu_no',
+        type=str,
+        default=None,
+        help='gpu device number')
 
-  # learning parameters and configs
-  parser.add_argument(
-      '--learning_rate', 
-      type=float, 
-      default=0.001,
-      help='initial learning rate')
-  parser.add_argument(
-      '--steps', 
-      type=int, 
-      default=1000,
-      help='step size')
-  parser.add_argument(
-      '--log_freq', 
-      type=int, 
-      default=1,
-      help='log frequency')
-  parser.add_argument(
-      '--train', 
-      type=bool, 
-      nargs='?',
-      default=False, #default
-      const=True, #if the arg is given
-      help='train or just eval')
+    # learning parameters and configs
+    parser.add_argument(
+        '--learning_rate',
+        type=float,
+        default=0.001,
+        help='initial learning rate')
+    parser.add_argument(
+        '--steps',
+        type=int,
+        default=1000,
+        help='step size')
+    parser.add_argument(
+        '--log_freq',
+        type=int,
+        default=1,
+        help='log frequency')
+    parser.add_argument(
+        '--train',
+        type=bool,
+        nargs='?',
+        default=False,  # default
+        const=True,  # if the arg is given
+        help='train or just eval')
 
-  FLAGS, unparsed = parser.parse_known_args()
-  
-  if FLAGS.gpu_no is not None:
-    os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu_no
-  else:
-    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-  
-  tf.app.run(main=main)
+    FLAGS, unparsed = parser.parse_known_args()
+
+    if FLAGS.gpu_no is not None:
+        os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu_no
+    else:
+        os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+    tf.app.run(main=main)
--- models.py
+++ models.py
@@ -5,119 +5,126 @@ from custom_loss import distance_loss
 
 
 def _variable_on_cpu(name, shape):
-  initializer = tf.contrib.layers.xavier_initializer(dtype=tf.float32)
-  with tf.device('/cpu:0'):
-    var = tf.get_variable(name, shape, initializer=initializer, dtype=tf.float32)
-  return var
+    initializer = tf.contrib.layers.xavier_initializer(dtype=tf.float32)
+    with tf.device('/cpu:0'):
+        var = tf.get_variable(name, shape, initializer=initializer, dtype=tf.float32)
+    return var
 
 
 def rnn_last_output(rnn_input, n_unit=16, bi_direction=False):
-  rnn_cell = rnn.BasicLSTMCell(n_unit)
-  if bi_direction:
-    outputs, _, _ = rnn.static_bidirectional_rnn(
+    rnn_cell = rnn.BasicLSTMCell(n_unit)
+    if bi_direction:
+        outputs, _, _ = rnn.static_bidirectional_rnn(
             rnn_cell, rnn_cell, rnn_input, dtype=tf.float32)
-  else:
-    outputs, _ = rnn.static_rnn(
+    else:
+        outputs, _ = rnn.static_rnn(
             rnn_cell, rnn_input, dtype=tf.float32)
-  return outputs[-1]
+    return outputs[-1]
 
 
 def embed_path(paths, params):
-  print('path embedded')
-  if params['model_type'] == 'dnn':
-    nn_inputs = tf.reshape(paths, shape=[-1, 4 * params['k']]) ###?
-    return tf.layers.dense(nn_inputs, 
-                           params['path_embedding_dim'], 
-                           activation=tf.nn.relu)
-  else:
-    rnn_inputs = tf.unstack(paths, axis=1, name='unstack')
-    return rnn_last_output(rnn_inputs, 
-                           n_unit=params['path_embedding_dim'], 
-                           bi_direction=params['bi_direction'])
+    print('path embedded')
+    if params['model_type'] == 'dnn':
+        nn_inputs = tf.reshape(paths, shape=[-1, 4 * params['k']])  ###?
+        return tf.layers.dense(nn_inputs,
+                               params['path_embedding_dim'],
+                               activation=tf.nn.relu)
+    else:
+        rnn_inputs = tf.unstack(paths, axis=1, name='unstack')
+        return rnn_last_output(rnn_inputs,
+                               n_unit=params['path_embedding_dim'],
+                               bi_direction=params['bi_direction'])
 
 
 def embed_meta(metas, params):
-  print('meta embedded')
-  holiday, weekno, hour, weekday = tf.split(metas, metas.shape[1], axis=1)
-  
-  holi_W = _variable_on_cpu('holiday_table', [2, 1])
-  holiday_lookup = tf.nn.embedding_lookup(holi_W, holiday, name='holiday_lookup')
-  holiday_embedding = tf.squeeze(holiday_lookup, axis=1, name='holiday_embedding')
+    print('meta embedded')
+    holiday, weekno, hour, weekday = tf.split(metas, metas.shape[1], axis=1)
 
-  # weekno_W = _variable_on_cpu('weekno_table', [53, 10])
-  # weekno_lookup = tf.nn.embedding_lookup(weekno_W, weekno, name='weekno_lookup')
-  # weekno_embedding = tf.squeeze(weekno_lookup, axis=1, name='weekno_embedding')
+    holi_W = _variable_on_cpu('holiday_table', [2, 1])
+    holiday_lookup = tf.nn.embedding_lookup(holi_W, holiday, name='holiday_lookup')
+    holiday_embedding = tf.squeeze(holiday_lookup, axis=1, name='holiday_embedding')
 
-  hour_W = _variable_on_cpu('hour_table', [24, 4])
-  hour_lookup = tf.nn.embedding_lookup(hour_W, hour, name='hour_lookup')
-  hour_embedding = tf.squeeze(hour_lookup, axis=1, name='hour_embedding')
+    # weekno_W = _variable_on_cpu('weekno_table', [53, 10])
+    # weekno_lookup = tf.nn.embedding_lookup(weekno_W, weekno, name='weekno_lookup')
+    # weekno_embedding = tf.squeeze(weekno_lookup, axis=1, name='weekno_embedding')
 
-  weekday_W = _variable_on_cpu('weekday_table', [7, 2])
-  weekday_lookup = tf.nn.embedding_lookup(weekday_W, weekday, name='weekday_lookup')
-  weekday_embedding = tf.squeeze(weekday_lookup, axis=1, name='weekday_embedding')
+    hour_W = _variable_on_cpu('hour_table', [24, 4])
+    hour_lookup = tf.nn.embedding_lookup(hour_W, hour, name='hour_lookup')
+    hour_embedding = tf.squeeze(hour_lookup, axis=1, name='hour_embedding')
 
-  return tf.concat([holiday_embedding, 
-                    # weekno_embedding, 
-                    hour_embedding, 
-                    weekday_embedding], axis=1)
+    weekday_W = _variable_on_cpu('weekday_table', [7, 2])
+    weekday_lookup = tf.nn.embedding_lookup(weekday_W, weekday, name='weekday_lookup')
+    weekday_embedding = tf.squeeze(weekday_lookup, axis=1, name='weekday_embedding')
+
+    return tf.concat([holiday_embedding,
+                      # weekno_embedding,
+                      hour_embedding,
+                      weekday_embedding], axis=1)
 
 
 def model_fn(features, labels, mode, params):
-  """Model function for Estimator."""
+    """Model function for Estimator."""
 
-  concat_target = []
+    concat_target = []
 
-  # META embedding
-  if params['use_meta']:
-    metas = features['meta']
-    meta_embedding = embed_meta(metas, params)
-    concat_target.append(meta_embedding)
+    # META embedding
+    if params['use_meta']:
+        metas = features['meta']
+        meta_embedding = embed_meta(metas, params)
+        concat_target.append(meta_embedding)
 
-  # PATH embedding  
-  if params['use_path']:
-    paths = features['path']
-    paths = tf.cast(paths, dtype=tf.float32)
-    path_embedding = embed_path(paths, params)
-    concat_target.append(path_embedding)
+    # PATH embedding
+    if params['use_path']:
+        paths = features['path']
+        paths = tf.cast(paths, dtype=tf.float32)
+        path_embedding = embed_path(paths, params)
+        concat_target.append(path_embedding)
 
-  x = tf.concat(concat_target, axis=1, name='concat_embedded_input')
+    x = tf.concat(concat_target, axis=1, name='concat_embedded_input')
 
-  # FINAL dense layer
-  for i_layer in range(1, params['n_hidden_layer'] + 1):
-    n_hidden_node = x.get_shape().as_list()[1]
-    x = tf.layers.dense(x, n_hidden_node, activation=tf.nn.relu)
-  predictions = tf.layers.dense(x, 2, activation=None)
+    # FINAL dense layer
+    for i_layer in range(1, params['n_hidden_layer'] + 1):
+        n_hidden_node = x.get_shape().as_list()[1]
+        x = tf.layers.dense(x, n_hidden_node, activation=tf.nn.relu)
+    if params['use_cluster']:
+        dest_centroids = features['dest_centroid']
+        n_centroids = dest_centroids.get_shape().as_list()[1]
+        x = tf.layers.dense(x, n_centroids, activation=tf.nn.relu)
+        x = tf.expand_dims(tf.nn.softmax(x), axis=1)
+        predictions = tf.squeeze(tf.matmul(x, dest_centroids))
+    else:
+        predictions = tf.layers.dense(x, 2, activation=None)
 
-  if mode == tf.estimator.ModeKeys.TRAIN:
-    print('Trainable Variables')
-    for v in tf.trainable_variables():
-      print(v.name + ':' + str(v.shape))
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        print('Trainable Variables')
+        for v in tf.trainable_variables():
+            print(v.name + ':' + str(v.shape))
 
-  # Provide an estimator spec for `ModeKeys.PREDICT`.
-  if mode == tf.estimator.ModeKeys.PREDICT:
+    # Provide an estimator spec for `ModeKeys.PREDICT`.
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            predictions=predictions)
+
+    # Define loss, optimizer, and train_op
+    labels = tf.cast(labels, dtype=tf.float32)
+    # loss = tf.losses.mean_squared_error(labels, predictions)
+    loss = distance_loss(labels, predictions)
+    # optimizer = tf.train.GradientDescentOptimizer(
+    optimizer = tf.train.AdamOptimizer(
+        learning_rate=params["learning_rate"])
+    train_op = optimizer.minimize(loss=loss,
+                                  global_step=tf.train.get_global_step())
+
+    # Calculate root mean squared error as additional eval metric
+    # eval_metric_ops = dict(
+    #     # rmse=tf.metrics.root_mean_squared_error(labels, predictions),
+    #     distance=distance_metric(labels, predictions)
+    # )
+
+    # Provide an estimator spec for `ModeKeys.EVAL` and `ModeKeys.TRAIN` modes.
     return tf.estimator.EstimatorSpec(
         mode=mode,
-        predictions=predictions)
-
-  # Define loss, optimizer, and train_op
-  labels = tf.cast(labels, dtype=tf.float32)
-  # loss = tf.losses.mean_squared_error(labels, predictions)
-  loss = distance_loss(labels, predictions)
-  # optimizer = tf.train.GradientDescentOptimizer(
-  optimizer = tf.train.AdamOptimizer(
-      learning_rate=params["learning_rate"])
-  train_op = optimizer.minimize(loss=loss, 
-                                global_step=tf.train.get_global_step())
-
-  # Calculate root mean squared error as additional eval metric
-  # eval_metric_ops = dict(
-  #     # rmse=tf.metrics.root_mean_squared_error(labels, predictions),
-  #     distance=distance_metric(labels, predictions)
-  # )
-
-  # Provide an estimator spec for `ModeKeys.EVAL` and `ModeKeys.TRAIN` modes.
-  return tf.estimator.EstimatorSpec(
-      mode=mode,
-      loss=loss,
-      train_op=train_op,
-      eval_metric_ops=None)
+        loss=loss,
+        train_op=train_op,
+        eval_metric_ops=None)
