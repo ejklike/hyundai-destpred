8033498558e65a3b72ed1f7ec1fb18e1d75c489f
ejklike <ejklike@gmail.com>
2017-11-10 17:33:00 +0900

    model_id, multiple rawdata, full_path

--- estimator.py
+++ estimator.py
@@ -1,12 +1,9 @@
 import argparse
-from itertools import islice, product
-import random
-import sys
+from itertools import product
 import os
-import pickle
 
 import numpy as np
-from sklearn.cluster import MeanShift, estimate_bandwidth
+from sklearn.cluster import MeanShift
 import tensorflow as tf
 
 from data_preprocessor import DataPreprocessor
@@ -21,18 +18,12 @@ from utils import (maybe_exist,
                    visualize_cluster,
                    visualize_predicted_destination)
 
-
-# TODO: Quick hack for loading model and inference. (via input_ftn???)
-# https://gist.github.com/Inoryy/b606aafea2e3faa3ca847d7be986c999
-
-FLAGS = None
-
 # Data dir
 DATA_DIR = './data_pkl'
 MODEL_DIR = os.path.join(os.getcwd(), './tf_models')
-VIZ_DIR = './viz'
+VIZ_DIR = './viz/test'
 
-RAW_DATA_FNAME = 'dest_route_pred_sample.csv'
+RAW_DATA_FNAME_LIST = ['dest_route_pred_sample.csv', 'dest_route_pred_sample_ag.csv']
 RECORD_FNAME = 'result.csv'
 
 # how-to-suppress-verbose-tensorflow-logging
@@ -53,8 +44,9 @@ def train_eval_save(car_id, proportion, dest_term,
       DATA_DIR,
       get_pkl_file_name(car_id, proportion, dest_term, train=False))
 
-  path_trn, meta_trn, dest_trn, _ = load_data(fname_trn, k=params['k'])
-  path_tst, meta_tst, dest_tst, dt_tst = load_data(fname_tst, k=params['k'])
+  path_trn, meta_trn, dest_trn, _, _ = load_data(fname_trn, k=params['k'])
+  path_tst, meta_tst, dest_tst, dt_tst, fpath_tst = load_data(fname_tst, k=params['k'])
+#   print([fpath for fpath in fpath_tst])
 
   # split train set into train/validation sets
   num_trn = int(len(path_trn) * (1 - FLAGS.validation_size))
@@ -88,13 +80,12 @@ def train_eval_save(car_id, proportion, dest_term,
     log.info('#cluster of destination = %d', n_cluster)
     params['cluster_centers'] = cluster_centers
     params['n_clusters'] = n_cluster
-  else:
-    cluster_centers = None
-
-  cluster_fname = '{}/cluster/car_{}__dest_{}__cband_{}.png'.format(
+    cluster_fname = '{}/cluster/car_{}__dest_{}__cband_{}.png'.format(
           VIZ_DIR, car_id, dest_term, params['cluster_bw'])
-  if FLAGS.model_type == 'dnn':
-    visualize_cluster(dest_trn, dest_val, dest_tst, cluster_centers, fname=cluster_fname)
+    if FLAGS.model_type == 'dnn':
+      visualize_cluster(dest_trn, dest_val, dest_tst, cluster_centers, 
+                        bandwidth=params['cluster_bw'], n_cluster=n_cluster,
+                        fname=cluster_fname)
 
   # input functions for evaluation
   eval_input_fn_trn = tf.estimator.inputs.numpy_input_fn(
@@ -194,7 +185,10 @@ def train_eval_save(car_id, proportion, dest_term,
             model_id=model_id, 
             start_dt=convert_time_for_fname(dt_tst[i]))
       visualize_predicted_destination(
-          path_tst[i], dest_tst[i], pred, fname=fname)
+          fpath_tst[i], 
+          meta_tst[i],
+          path_tst[i] if proportion > 0 else None, 
+          dest_tst[i], pred, fname=fname)
 
   return trn_err, val_err, tst_err
 
@@ -206,45 +200,43 @@ def main(_):
   # Preprocess data: convert to pkl data
   if FLAGS.preprocess:
     maybe_exist(DATA_DIR)
-    data_preprocessor = DataPreprocessor(RAW_DATA_FNAME)
-    data_preprocessor.process_and_save(save_dir=DATA_DIR)
-
-    # (1) 주행경로 길이의 평균
-    # (2) 주행 횟수
-    #
-    #       (1)    (2)
-    #   5: 상위권 하위권
-    # 100: 상위권 하위권
-    #  29: 상위권 하위권
-    #  72: 하위권 상위권
-    #  50: 하위권 상위권
-    #  14:  평균  상위권
-    #   9:  평균   평균
-    #  74:  평균   평균
+    for raw_data_fname in RAW_DATA_FNAME_LIST:
+        data_preprocessor = DataPreprocessor(to_dir=DATA_DIR)
+        data_preprocessor.process_and_save(raw_data_fname)
 
   # training target cars
-  # car_id_list = [FLAGS.car_id] if FLAGS.car_id is not None else [72, 74, 100]# [5, 9, 14, 29, [50, 72, 74, 100]
-  car_id_list = [22, 23, 24, 25, 26, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 39, 42, 43, 44, 45, 46, 47, 49, 50, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 80, 81, 82, 83, 84, 85, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 100, ]#[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 39, 42, 43, 44, 45, 46, 47, 49, 50, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 80, 81, 82, 83, 84, 85, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 100, ] # all
-  # car_id_list = [1, 2, 3, 4, 6, 7, 8, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 28, 30, 31, 32, 33, 34, 35, 36, 37, 39, 42, 43, 44, 45, 46, 47, 49, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 64, 65, 66, 67, 68, 69, 70, 71, 73, 75, 76, 77, 78, 80, 81, 82, 83, 84, 85, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, ] # complement
-  # input path specification
-  proportion_list = [FLAGS.proportion] if FLAGS.proportion is not None else [0.2, 0.4, 0.6, 0.8]
-  use_meta_path = [(True, True), (True, False), (False, True)]
+  # car_id_list = [
+  # 'KMH', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 
+  # 19, 20, 21, 22, 23, 24, 25, 26, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 
+  # 39, 42, 43, 44, 45, 46, 47, 49, 50, 52, 53, 54, 55, 56, 57, 58, 59, 60, 
+  # 61, 62, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 80, 
+  # 81, 82, 83, 84, 85, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 100, 
+  # ] # all
+  car_id_list = [FLAGS.car_id] if FLAGS.car_id is not None else [
+    'KMH', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 
+  19, 20, 21, 22, 23, 24, 25, 26, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 
+  39, 42, 43, 44, 45, 46, 47, 49, 50, 52, 53, 54, 55, 56, 57, 58, 59, 60, 
+  61, 62, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 
+  ]
+#   car_id_list = ['KMH']
 
-  # destination specification
-  short_term_dest_list = [FLAGS.dest_type] if FLAGS.dest_type is not None else [-1, 5]
-  print(FLAGS.cband)
-  cluster_bw_list = [FLAGS.cband] if FLAGS.cband is not None else [0, 0.01, 0.1, 0.3]
+  # input path specification
+  short_term_dest_list = [0, 5] if FLAGS.dest_type is None else [FLAGS.dest_type]
+  proportion_list = [0.0, 0.2, 0.4, 0.6, 0.8] if FLAGS.proportion is None else [FLAGS.proportion]
 
   # Used for loading data and building graph
-  k_list = [5] if FLAGS.model_type == 'dnn' else [0]          #, 10, 15, 20]
+  use_meta_list = [True, False]
+  k_list = [5] if FLAGS.model_type == 'dnn' else [0]
   path_embedding_dim_list = [FLAGS.path_dim] if FLAGS.path_dim is not None else [10, 50, 100]
+
   n_hidden_layer_list = [FLAGS.n_dense] if FLAGS.n_dense is not None else [1, 2, 3]
+  cluster_bw_list = [FLAGS.cband] if FLAGS.cband is not None else [0, 0.01, 0.1, 0.3]
 
   # PARAM GRIDS
   param_grid_targets = [car_id_list,
                         short_term_dest_list, # destination
-                        use_meta_path, # meta or path or both
                         proportion_list, # path only
+                        use_meta_list, # 
                         path_embedding_dim_list, # path only
                         k_list, # path dnn only
                         n_hidden_layer_list, # for final dense layers
@@ -254,29 +246,25 @@ def main(_):
   param_product_size = np.prod([len(t) for t in param_grid_targets])
 
   for i, params in enumerate(param_product):
-    car_id, dest_term, (use_meta, use_path) = params[:3]
-    proportion, path_embedding_dim, k = params[3:6]
+    car_id, dest_term, proportion, use_meta, path_embedding_dim, k = params[:6]
     n_hidden_layer, cluster_bw = params[6:]
 
     # If we do not use path input,
     # some param grids are not needed.
-    if use_path is False:
+    if proportion == 0:
       if FLAGS.model_type == 'rnn': # train meta setting only in DNN run
         continue
-      if proportion > proportion_list[0]:
+      if (FLAGS.model_type == 'dnn') and (k != k_list[0]):
         continue
-      if (FLAGS.model_type == 'dnn') and (k > k_list[0]):
+      if use_meta is False:
         continue
-      # this params are useless
-      path_embedding_dim = 0
-      # proportion = 0.2 -> These params are used only for importing data.
-      # k = 5            -> In model_id, they are set to zero.
+      k = 0 # set to 0 after continue statements. this param will be used only for importing data
 
     model_params = dict(
         learning_rate=FLAGS.learning_rate,
         # feature_set
         use_meta=use_meta,
-        use_path=use_path,
+        use_path=True if proportion > 0 else False,
         # model type
         model_type=FLAGS.model_type,
         cluster_bw=cluster_bw,
@@ -290,18 +278,17 @@ def main(_):
         n_hidden_layer=n_hidden_layer,
     )
 
-    # Model id
-    id_components = ['car{:03}'.format(car_id),
-                     'dest{}'.format(dest_term if dest_term > 0 else 0),
-                     ''.join(['meta' if use_meta else '', 
-                              'path_prop_{prop}_to_{model}_{edim}'.format(
-                                  prop=proportion,
-                                  model=''.join([
-                                      'b' if FLAGS.bi_direction else '',
-                                      FLAGS.model_type,
-                                      '_k_%d' %k if FLAGS.model_type == 'dnn' else '']),
-                                  edim=path_embedding_dim) if use_path else '']),
-                     'dense_{}'.format(n_hidden_layer),
+    # Model id 'meta' if use_meta else '', 
+    id_components = [('car{:03}' if isinstance(car_id, int) else 'car{}').format(car_id),
+                     'dest{:02}'.format(dest_term if dest_term > 0 else 0),
+                     'path{:.1f}'.format(proportion),
+                     'meta_only' if proportion == 0 else '{model}_{edim}'.format(
+                          model=''.join([
+                              'meta_' if use_meta is True else '',
+                              'b' if FLAGS.bi_direction else '', FLAGS.model_type,
+                              '_k%d' % k if FLAGS.model_type == 'dnn' else '']),
+                          edim=path_embedding_dim),
+                     'dense_{}x{}'.format(path_embedding_dim, n_hidden_layer),
                      'cband_{}'.format(cluster_bw)]
     model_id = '__'.join(id_components)
 
@@ -437,6 +424,7 @@ if __name__ == "__main__":
   FLAGS, unparsed = parser.parse_known_args()
 
   print(FLAGS.proportion, FLAGS.car_id, FLAGS.dest_type, FLAGS.cband, FLAGS.path_dim, FLAGS.n_dense)
+  print(unparsed)
 
   
   if FLAGS.gpu_no is not None:
