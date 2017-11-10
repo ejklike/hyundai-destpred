0307fd1fb8be6708a451c1ede0636786b273cbf1
ejklike <ejklike@gmail.com>
2017-11-10 17:30:52 +0900

    add a function that vizualize the density of destination

--- utils.py
+++ utils.py
@@ -17,16 +17,20 @@ def maybe_exist(directory):
         os.makedirs(directory)
 
 
+def convert_str_to_time(date_time):
+    return datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
+
+
 def convert_time_for_fname(date_time):
     if isinstance(date_time, str):
-        date_time = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
+        date_time = convert_str_to_time(date_time)
     return date_time.strftime('%Y%m%d_%H%M%S')
 
 
 def dist(x, y, to_km=False):
     rad_to_km = np.array([111.0, 88.8])
     
-    x, y = x.reshape(-1, 2), y.reshape(-1, 2)
+    x, y = np.array(x).reshape(-1, 2), np.array(y).reshape(-1, 2)
     delta_square = (x - y)**2
     if to_km is True:
         delta_square *= rad_to_km**2
@@ -56,7 +60,8 @@ def load_data(fname, k=0):
       dests: nparray of shape [data_size, 2]
   """
   data = pickle.load(open(fname, 'rb'))
-  paths, metas, dests, dts = data['path'], data['meta'], data['dest'], data['dt']
+  paths, metas, dests = data['path'], data['meta'], data['dest']
+  full_paths, dts = data['full_path'], data['dt']
 
   if k == 0: # RNN or NO PATH
     def resize_by_padding(path, target_length):
@@ -86,7 +91,7 @@ def load_data(fname, k=0):
   
   metas, dests = np.array(metas), np.array(dests)
 
-  return paths, metas, dests, dts
+  return paths, metas, dests, dts, full_paths
 
 
 def record_results(fname, model_id, trn_size, val_size, tst_size, 
@@ -150,25 +155,134 @@ def visualize_cluster(dest_trn, dest_val, dest_tst, centers, fname=None, **kwarg
     plt.savefig(fname); plt.close()
 
 
-def visualize_predicted_destination(x, y_true, y_pred, fname=None):
+def visualize_dest_density(dest_trn, dest_tst, segment=10, fname=None, **kwargs):
     if fname is None:
         raise ValueError('You must enter the fname!')
 
-    if len(x.shape) == 1:
-        path = x.reshape(-1, 2) # from 1d to 2d data
+    #min/max of plot axes
+    all_points = np.concatenate([dest_trn, dest_tst], axis=0)
+    xmin, ymin = np.min(all_points, axis=0)
+    xmax, ymax = np.max(all_points, axis=0)
+
+    # add some margin
+    dx, dy = xmax-xmin, ymax-ymin
+    xmin, xmax = xmin - dx/10, xmax + dx/10
+    ymin, ymax = ymin - dy/10, ymax + dy/10
+    
+    #meshgrid for scoring
+    nx, ny = (100, 100)
+    dx, dy = (xmax-xmin)/nx, (ymax-ymin)/ny
+    xgrid = np.linspace(xmin, xmax, nx)
+    ygrid = np.linspace(ymin, ymax, ny)
+    gridX, gridY = np.meshgrid(xgrid, ygrid) #meshgrid X, Y
+        
+    # print('score evaluation...')
+    gridInput = np.vstack([gridX.ravel(), gridY.ravel()]).T #input 2d vectors for scoring
+    bandwidth = dist((xmin, ymin), (xmax, ymax), to_km=False) / 20
+    bandwidth_km = dist((xmin, ymin), (xmax, ymax), to_km=True) / 20
+    kde = KernelDensity(kernel='gaussian', bandwidth=bandwidth).fit(dest_trn)
+    grid_score = np.exp(kde.score_samples(gridInput))
+    grid_score = grid_score.reshape(gridX.shape)
+
+    min_score = np.amin(grid_score)
+    max_score = np.amax(grid_score)        
+    # print('---the sum of score =', dx * dy * np.sum(grid_score))
+    # print('---the range of score = ({}, {})'.format(min_score, max_score))
+
+    #plot contour
+    fig, ax = plt.subplots()
+    ax.set_xlim([ymin, ymax])
+    ax.set_ylim([xmin, xmax])
+    cmap = plt.get_cmap('Blues')
+    levels = np.linspace(min_score, max_score, segment)
+    cs = ax.contourf(gridY, gridX, grid_score, levels=levels, cmap=cmap, origin='upper')
+    fig.colorbar(cs, ax=ax, shrink=0.9)
+
+    #show bandwidth length
+    dx, dy = xmax-xmin, ymax-ymin
+    lbottom = [xmin + 0.1 * dx, ymin + 0.05 * dy]
+    rbottom = [xmin + 0.1 * dx, ymax - 0.05 * dy]
+    lscore, rscore = np.exp(kde.score_samples([lbottom, rbottom]))
+    if lscore > rscore:
+        tx, ty = rbottom
+        ty_end, ty_text = ty - bandwidth, ty - 0.9 * bandwidth
     else:
-        path = x[np.sum(x, axis=1) != 0, :] # remove zero paddings
+        tx, ty = lbottom
+        ty_end, ty_text = ty + bandwidth, ty + 0.1 * bandwidth
+    
+    ax.plot([ty_end, ty], [tx, tx], 'k-', lw=2)
+    ax.annotate('%.2f' % bandwidth, xy=[ty, tx], xytext=[ty_text, tx - 0.05 * dx])
 
     # data, label, color, marker
     # colorname from https://matplotlib.org/examples/color/named_colors.html
-    path_data_list = [ 
-        ('input_path', path, 'mediumblue', '.'),
+    data_list = [
+        ('destinations (trn)', dest_trn, 'slategray', 'o', 0.3),
+        ('destinations (tst)', dest_tst, 'orangered', '.', 0.5),
     ]
+    for label, path, color, marker, alpha in data_list:
+        if path is not None:
+            ax.scatter(path[:, 1], path[:, 0], 
+                       c=color, marker=marker, label=label, alpha=alpha)
+    ax.legend(); ax.grid(True)
+
+    fname_without_extension = fname[:-4]
+    *save_dir, fname_without_dir = fname_without_extension.split('/')
+    maybe_exist('/'.join(save_dir))
+    car, dest, *_ = fname_without_dir.split('__')
+    title = '{car}, {dest}{setting}'.format(
+        car=car.upper(),
+        dest='FINAL DEST' if dest[-1] == '0' else 'DEST AFTER {} MIN.'.format(dest[-1]),
+        setting='' if not kwargs 
+                else ('\n(' + ', '.join(['{}={}'.format(k, v) for k, v in kwargs.items()]) + ')'))
+        # setting='' if centers is None else '\n(cband=%d, n_centers=%d)'%(cband, len(centers)))
+    title += '\n(bandwidth_rad={rad:.2f}, bandwidth_km={km:.1f})'.format(
+             rad=bandwidth, km=bandwidth_km)
+
+    ax_title = ax.set_title(title)
+    fig.subplots_adjust(top=0.8)
+    plt.xlabel('longitude (translated)')
+    plt.ylabel('latitude (translated)')
+    plt.savefig(fname); plt.close()
+
+
+def visualize_predicted_destination(full_path, meta, x, y_true, y_pred, fname=None):
+    if fname is None:
+        raise ValueError('You must enter the fname!')
+
+    meta_str = '{weekday}{holiday}, {hour:00}h'.format(
+        weekday={0:'MON', 1:'TUE', 2:'WED', 3:'THU', 4:'FRI', 5:'SAT', 6:'SUN'}[meta[3]], 
+        holiday='(h)' if meta[0] == 1 else '', 
+        hour=meta[2]
+    )
+
+    # data, label, color, marker
+    # colorname from https://matplotlib.org/examples/color/named_colors.html
     point_data_list = [
-        ('starting_point', path[0], 'mediumblue', '.'),
         ('true_destination', y_true, 'mediumblue', '*'),
         ('pred_destination', y_pred, 'crimson', '*'),
+        (meta_str, [-100, -100], 'white', ''),
     ]
+    path_data_list = [ 
+        ('full_path', full_path, 'lightgrey', '.'),
+    ]
+    if x is not None:
+        if len(x.shape) == 1:
+            path = x.reshape(-1, 2) # from 1d to 2d data
+            full_path = full_path.reshape(-1, 2)
+            path_data_list.append(
+                ('model_input', path[:len(path)//2], 'mediumblue', '.')
+            )
+            path_data_list.append(
+                (None, path[len(path)//2:], 'mediumblue', '.')
+            )
+        else:
+            path = x[np.sum(x, axis=1) != 0, :] # remove zero paddings
+            path_data_list.append(
+                ('model_input', path, 'mediumblue', '.')
+            )
+        # point_data_list.append(
+        #     ('starting_point', path[0], 'mediumblue', '.')
+        # )
 
     fig, ax = plt.subplots()
     for label, path, color, marker in path_data_list:
